5a606810a617d25443e77a6ffb4ee52d8eceb82b
acer <acer@172.16.98.231>
2019-04-25 14:45:06 +0700

    UTS

--- src/utspbo/main.java
+++ src/utspbo/main.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package utspbo;
 
-/**
- *
- * @author acer
- */
+
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         Intel intel1 = new Intel(3);
         AMD amd1 = new AMD(3);
